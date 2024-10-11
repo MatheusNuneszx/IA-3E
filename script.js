dff64cf6ef42f2b985b2555d168799d93d9aa60f
MatheusNuneszx <110679454+MatheusNuneszx@users.noreply.github.com>
2024-10-11 09:19:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,54 +2,45 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
-const perguntas = [//abre a lista de objetos (itens)
-    {//abre o item
-        enunciado: "Você gosta da Inteligência Artificial?",
-        alternativas: [{
-            texto: "Sim",
-            afirmação: "Porque sim"
-        },
-        {
-            texto: "Não",
-            afirmação: "Porque não"
-        }
-        ]
+const perguntas = [     //serve para abrir lista de perguntas
+    {   //abre o objeto das perguntas
+        enunciado: "Quem tem mais Bola de Ouro?",
+        alternativas: [
+            {texto: "Messi",
+            afirmação:"correta resposta"}, 
+
+            {texto: "CR7",
+            afirmação:"errou foi quase"}]
     },
-    {
-        enunciado: "O que você acha das IAs?",
-        alternativas: [{
-            texto: "Boas",
-            afirmação: "É top, usso para pesquisas"
-        },
-        {
-            texto:"Ruins",
-            afirmação:"É ruim mesmo"
-        }
-        ]
+    { 
+        enunciado: "Quem ganhou a última Copa do Mundo?",
+        alternativas: [
+            {texto: "Argentina",
+            afirmação:"ótimo"}, 
+                
+            {texto: "França",
+            afirmação:"tenho que treinar"}]
     },
-    {
-        enunciado: "Você usa as IAs com frequência?",
-        alternativas: [{
-            texto: "Sim",
-            afirmação: "Uso direto"
-        },
-        {
-            texto: "Não",
-            afirmação: "Quase nunca uso"
-        }
-        ]
-    }
+    { 
+        enunciado: "Brasil tem quantas Copas do Mundo?",
+        alternativas: [
+            {texto: "7",
+            afirmação:"sou ruim"}, 
+                
+            {texto: "5",
+            afirmação:"sou muito bom"}]
+    },
 ]
 let posicao = 0;
 let perguntaAtual;
-let respostas= = "";
+let respostas = "";
 
 
 function mostraPergunta() {
     if (posicao>=perguntas.length){
         mostraResultado();
-        mostraAlternativas();
         return;
     }
     perguntaAtual = perguntas[posicao];
@@ -61,19 +52,19 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click",  () => respostaSelecionadas(alterntiva));
+        botaoAlternativas.addEventListener("click",  () => respostasSelecionadas(alternativa));
         caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
 function respostasSelecionadas(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afimacao;
+    const afirmacoes = opcaoSelecionada.afirmação;
     respostas += afirmacoes + " ";
     posicao++;
     mostraPergunta();
 }
 function mostraResultado(){
-    caixaPergunta.textContent = "Daqui a 10 anos...";
-    textoResultado.textoContent = respostas;
-    caixaAlternativa.textContent = ""
+    caixaPergunta.textContent = "Confira suas respostas: ";
+    textoResultado.textContent = respostas; 
+    caixaAlternativa.textContent = "";
 }
 mostraPergunta();
